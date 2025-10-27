ed92e888795fdee74518ffa9a43683f27c0f535f
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-10-28 00:22:34 +0200

    add this to my repository

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -1,3 +1,18 @@
 class PlaceFinder {
-    
+    constructor(){
+
+        //here we are selecting elements from the DOM
+        const addressForm = document.queryselector('form');
+        const locateUserBtn = document.queryselector('locate-btn');
+
+        //adding event listeners to the elements
+        locateUserBtn.addEventListener('click', this.locateUserHandler); 
+    }    
+
+    //creating the locateUserHandler method that will be called when the button is clicked
+    locateUserHandler(){
+
+    }
 }
+
+
