d0b5da114fe10de529d3326d83a18037e827e662
gabriel buhendwa <gabrielbuhendwa400@gmail.com>
2025-10-28 00:32:05 +0200

    add this to my repository

--- src/SharePlace.js
+++ src/SharePlace.js
@@ -7,11 +7,19 @@ class PlaceFinder {
 
         //adding event listeners to the elements
         locateUserBtn.addEventListener('click', this.locateUserHandler); 
-    }    
+        //when the button is clicked, the locateUserHandler method will be called
+        addressForm.addEventListener('submit', this.findAddressHandler);
+
+    }   
+
 
     //creating the locateUserHandler method that will be called when the button is clicked
     locateUserHandler(){
 
+    }
+    //creating the findAddressHandler method that will be called when the form is submitted
+    findAddressHandler(){
+        
     }
 }
 
